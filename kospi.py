6377b19de163f36669e1178c2c2cf2e2c1801fa7
manuck <snc9000@naver.com>
2018-12-18 15:13:35 +0900

    네이버 코스피 크롤링

---
+++ kospi.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://finance.naver.com/sise/"
+#1. 원하는 사이트에 요청을 보낸다.
+# 그리고, 그 결과를 response에 저장한다.
+response = requests.get(url)
+##print(response.text)
+##print(type(response))
+
+#2. 원하는 정보를 찾는다.
+soup = BeautifulSoup(response.text, 'html.parser')
+#3. select는 css의 선택자(selector)를 통해 찾을 수 있다.
+# #KOSPI_now : id가 KOSPI_now인 것.
+# .up : class가 up인 것
+# CSS에서 id는 문서에서 하나, class는 여러개가 있을 수 있다.
+kospi =  soup.select_one('#KOSPI_now')
+print(kospi.text)
